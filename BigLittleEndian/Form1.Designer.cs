5038c770b19681bce07d6358d8f92cb2c40da15c
iammark666 <ning328534965@126.com>
2021-09-11 12:23:30 +0800

    Initial commit. Version 1.0
    BigSmallEndingTranform

---
+++ BigLittleEndian/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace BigLittleEndian
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BigEndian = new System.Windows.Forms.TextBox();
+            this.LittleEndian = new System.Windows.Forms.TextBox();
+            this.Label_BigEnd = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Big2Lit = new System.Windows.Forms.Button();
+            this.Lit2Big = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BigEndian
+            // 
+            this.BigEndian.Location = new System.Drawing.Point(97, 95);
+            this.BigEndian.Multiline = true;
+            this.BigEndian.Name = "BigEndian";
+            this.BigEndian.Size = new System.Drawing.Size(213, 238);
+            this.BigEndian.TabIndex = 0;
+            this.BigEndian.TextChanged += new System.EventHandler(this.BigEndian_TextChanged);
+            // 
+            // LittleEndian
+            // 
+            this.LittleEndian.Location = new System.Drawing.Point(485, 95);
+            this.LittleEndian.Multiline = true;
+            this.LittleEndian.Name = "LittleEndian";
+            this.LittleEndian.Size = new System.Drawing.Size(210, 238);
+            this.LittleEndian.TabIndex = 0;
+            this.LittleEndian.TextChanged += new System.EventHandler(this.LittleEndian_TextChanged);
+            // 
+            // Label_BigEnd
+            // 
+            this.Label_BigEnd.AutoSize = true;
+            this.Label_BigEnd.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_BigEnd.Location = new System.Drawing.Point(170, 62);
+            this.Label_BigEnd.Name = "Label_BigEnd";
+            this.Label_BigEnd.Size = new System.Drawing.Size(80, 16);
+            this.Label_BigEnd.TabIndex = 1;
+            this.Label_BigEnd.Text = "BigEndian";
+            this.Label_BigEnd.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(533, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LittleEndian";
+            // 
+            // Big2Lit
+            // 
+            this.Big2Lit.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Big2Lit.Location = new System.Drawing.Point(357, 135);
+            this.Big2Lit.Name = "Big2Lit";
+            this.Big2Lit.Size = new System.Drawing.Size(75, 45);
+            this.Big2Lit.TabIndex = 2;
+            this.Big2Lit.Text = "Big2Lit";
+            this.Big2Lit.UseVisualStyleBackColor = true;
+            this.Big2Lit.Click += new System.EventHandler(this.Big2Lit_Click);
+            // 
+            // Lit2Big
+            // 
+            this.Lit2Big.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Lit2Big.Location = new System.Drawing.Point(357, 224);
+            this.Lit2Big.Name = "Lit2Big";
+            this.Lit2Big.Size = new System.Drawing.Size(75, 41);
+            this.Lit2Big.TabIndex = 2;
+            this.Lit2Big.Text = "Lit2Big";
+            this.Lit2Big.UseVisualStyleBackColor = true;
+            this.Lit2Big.Click += new System.EventHandler(this.Lit2Big_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 435);
+            this.Controls.Add(this.Lit2Big);
+            this.Controls.Add(this.Big2Lit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label_BigEnd);
+            this.Controls.Add(this.LittleEndian);
+            this.Controls.Add(this.BigEndian);
+            this.Name = "Form1";
+            this.Text = "BigLitEndianTool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox BigEndian;
+        private System.Windows.Forms.TextBox LittleEndian;
+        private System.Windows.Forms.Label Label_BigEnd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Big2Lit;
+        private System.Windows.Forms.Button Lit2Big;
+    }
+}
+
---
+++ BigLittleEndian/Form1.cs
@@ -0,0 +1,107 @@
+﻿/*  BigLittleEndianTool
+ *  Version 1.0
+ *  2021年9月10日
+ *  Zhao Mark
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BigLittleEndian
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Big2Lit_Click(object sender, EventArgs e)
+        {
+
+                    //LittleEndian.Text = BigEndian.Text ;
+                    //MessageBox.Show(Convert.ToString(BigEndian.Lines.Length));
+
+            for (int i = 0; i < BigEndian.Lines.Length; i++)
+            {
+                    //MessageBox.Show(BigEndian.Lines[i]);
+                string b = BigEndian.Lines[i].Substring(0, 2);
+                    //MessageBox.Show(b);
+                string c = BigEndian.Lines[i].Substring(2, 2); 
+
+                string d = BigEndian.Lines[i].Substring(4, 2);
+                string f = BigEndian.Lines[i].Substring(6, 2);
+
+                /*  将text追加到LittleEndian.Text    */
+
+                if (i == 0)
+                {
+                    LittleEndian.Text = f + d + c + b;
+                }
+                else
+                {
+                    LittleEndian.Text += "\r\n" + f + d + c + b;
+                }
+            }
+            //MessageBox.Show(LittleEndian.Text);
+
+                    //MessageBox.Show(Convert.ToString(j));
+                    //MessageBox.Show(str_out[0]);
+                    //MessageBox.Show(BitConverter.ToString(str_temp));
+
+
+
+        }
+
+        private void BigEndian_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LittleEndian_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lit2Big_Click(object sender, EventArgs e)
+        {
+
+            //BigEndian.Text = LittleEndian.Text;
+
+            for (int i = 0; i < LittleEndian.Lines.Length; i++)
+            {
+                string b = LittleEndian.Lines[i].Substring(0, 2);
+                //MessageBox.Show(b);
+                string c = LittleEndian.Lines[i].Substring(2, 2); 
+
+                string d = LittleEndian.Lines[i].Substring(4, 2);
+                string f = LittleEndian.Lines[i].Substring(6, 2);
+
+                /*  将text追加到 BigEndian.Text    */
+
+                if (i == 0)
+                {
+                    BigEndian.Text = f + d + c + b;
+                }
+                else
+                {
+                    BigEndian.Text += "\r\n" + f + d + c + b;
+                }
+            }
+            //MessageBox.Show(LittleEndian.Text);
+
+        }
+    }
+}
